e609f8048b3c5fb125840fe2b63865712466fddf
Oleksandr_Kobzar <oleksandrkobzar@gmail.com>
2026-07-01 21:12:46 +0300

    Update

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,11 +1,11 @@
 // Middleware для обробки помилок (останнє)
-import { HttpError } from 'http-errors';
+import { isHttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
   console.error('Error Middleware:', err);
 
   // Якщо помилка створена через http-errors
-  if (err instanceof HttpError) {
+  if (isHttpError(err)) {
     return res.status(err.status).json({
       message: err.message || err.name,
     });
