bc256acb34540cd20c7cba9dce1295742c4d4389
Oleksandr_Kobzar <oleksandrkobzar@gmail.com>
2026-07-12 19:11:07 +0300

    Update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,11 +6,13 @@ import {
   refreshUserSession,
   registerUser,
   requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 import {
   loginUserSchema,
   registerUserSchema,
   requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -27,6 +29,12 @@ router.post(
   '/auth/request-reset-email',
   celebrate(requestResetEmailSchema),
   requestResetEmail,
+);
+
+router.post(
+  '/auth/reset-password',
+  celebrate(resetPasswordSchema),
+  resetPassword,
 );
 
 export default router;
